9b489100c80a059e58d1eaf8f69f04c9c46dd1c5
Derrick See <dsee@jenniferfurniture.com>
2016-10-28 07:40:27 -0400

    Improved shopify tasks

--- schedule/tasks.py
+++ schedule/tasks.py
@@ -19,60 +19,80 @@ def run_schedule():
 @shared_task
 def execute_change(variant_shopify_id, changes):
     try:
-        variant = shopify.Variant.find(variant_shopify_id)
-        if variant:
-            variant.compare_at_price = float(changes['compare_at_price'])
-            variant.price = float(changes['sale_price'] or changes['price'])
-            result = variant.save()
-            if result:
-                v = Variant.objects.get(id=changes['variant'])
-                v.compare_at_price = changes['compare_at_price']
-                v.price = changes['price']
-                v.sale_price = changes['sale_price']
-                v.save()
-                Schedule.update_status(changes['schedule'], result)
+        variant = shopify.Variant({
+            'id': variant_shopify_id,
+            'compare_at_price': float(changes['compare_at_price']),
+            'price': float(changes['sale_price'] or changes['price'])
+        })
+        result = variant.save()
+        if result:
+            Variant.objects.filter(id=changes['variant']).update(
+                compare_at_price = changes['compare_at_price'],
+                price = changes['price'], sale_price = changes['sale_price']
+            )
+            Schedule.update_status(changes['schedule'], result)
     except Exception as e:
         self.retry(exc=e, countdown=60)
 
 
 @shared_task
 def discount_product(product, schedule):
-    s_product = shopify.Product.find(product['shopify_id'])
-    if s_product and 'EXCLUDE' not in s_product.tags:
-        variants = Variant.objects.filter(product_id=product['id']).values('shopify_id', 'price')
-        v_map = {}
-        for d in variants:
-            v_map[d['shopify_id']] = d['price']
-        for v in s_product.variants:
-            if 'clearance' in s_product.tags:
-                discount = schedule['clearance_discount']
-            else:
-                discount = schedule['discount']
-            discount = Decimal('1.00') - (Decimal(discount) / Decimal(100))
-            price = (v_map[v.id] * discount).quantize(
+    try:
+
+        s_product = shopify.Product({
+            'id': product['shopify_id'],
+            'variants': [],
+        })
+        variants = Variant.objects.filter(product_id=product['id'])
+        discount = schedule['discount']
+        discount = Decimal('1.00') - (Decimal(discount) / Decimal(100))
+        for variant in variants:
+            price = (variant.price * discount).quantize(
                 Decimal('1.'), rounding=ROUND_UP) - Decimal('0.01')
-            v.price = float(price)
+            s_product.variants.append({
+                'id': variant.shopify_id,
+                'price': float(price)
+            })
         result = s_product.save()
-        Schedule.update_status(schedule['id'], result)
+        if result:
+            Schedule.update_status(schedule['id'], result)
+    except Exception as e:
+        self.retry(exc=e, countdown=60)
+
+    # if s_product and 'EXCLUDE' not in s_product.tags:
+    #     variants = Variant.objects.filter(product_id=product['id']).values('shopify_id', 'price')
+    #     v_map = {}
+    #     for d in variants:
+    #         v_map[d['shopify_id']] = d['price']
+    #     for v in s_product.variants:
+    #         if 'clearance' in s_product.tags:
+    #             discount = schedule['clearance_discount']
+    #         else:
+    #             discount = schedule['discount']
+    #         discount = Decimal('1.00') - (Decimal(discount) / Decimal(100))
+    #         price = (v_map[v.id] * discount).quantize(
+    #             Decimal('1.'), rounding=ROUND_UP) - Decimal('0.01')
+    #         v.price = float(price)
+    #     result = s_product.save()
+    #     Schedule.update_status(schedule['id'], result)
 
 
 @shared_task(rate_limit=1)
 def restore_product(product, schedule_id=None):
     try:
-        s_product = shopify.Product.find(product['shopify_id'])
-        if s_product:
-            variants = Variant.objects.filter(
-                product_id=product['id'])
-            v_map = {}
-            for d in variants.values('shopify_id', 'price'):
-                v_map[d['shopify_id']] = d['price']
-            for v in s_product.variants:
-                v.price = float(v_map[v.id])
-            result = s_product.save()
-            if result and schedule_id:
-                Schedule.update_status(schedule_id, result)
-                variants.update(sale_price=Decimal('0.00'))
-            return result
+        variants = Variant.objects.filter(product_id=product['id'])
+        v_map = {}
+        for d in variants.values('shopify_id', 'price'):
+            v_map[d['shopify_id']] = float(d['price'])
+        s_product = shopify.Product({
+            'id': product['shopify_id'],
+            'variants': v_map
+        })
+        result = s_product.save()
+        if result and schedule_id:
+            Schedule.update_status(schedule_id, result)
+            variants.update(sale_price=Decimal('0.00'))
+        return result
     except Exception as e:
         self.retry(exc=e, countdown=60)
 
@@ -80,8 +100,7 @@ def restore_product(product, schedule_id=None):
 @shared_task
 def update_theme(theme_id, schedule_id=None):
     try:
-        theme = shopify.Theme.find(theme_id)
-        theme.role = 'main'
+        theme = shopify.Theme({'id': theme_id, 'role': 'main'})
         result = theme.save()
         if result and schedule_id:
             Schedule.update_status(schedule_id, result)
