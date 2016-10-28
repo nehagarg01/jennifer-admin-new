73e21e2cf02de68b551782e91a3cbbc437cadc80
Derrick See <dsee@jenniferfurniture.com>
2016-10-28 14:39:44 -0400

    Fix js files
    Change signal to product update

--- webhooks/signals.py
+++ webhooks/signals.py
@@ -1,10 +1,10 @@
 from django.dispatch import receiver
 
-from shopify_webhook.signals import products_create
+from shopify_webhook.signals import products_update
 from products.utils import shopify
 from products.models import Product
 
 
-@receiver(products_create)
+@receiver(products_update)
 def update_product(sender, data, **kwargs):
     Product.shopify_sync(data)
