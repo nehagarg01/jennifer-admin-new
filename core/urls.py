c3a75573a95ab49e0adf1dc21671a5e31e6fd37e
Derrick See <dsee@jenniferfurniture.com>
2016-11-02 08:52:21 -0400

    Carrier Test

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 from dashboard.views import Dashboard
+from .views import CarrierView
 
 
 urlpatterns = [
@@ -16,4 +17,5 @@ urlpatterns = [
     url(r'^schedule/', include('schedule.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^webhooks/', WebhookView.as_view(), name="webhooks"),
+    url(r'^carrier/$', CarrierView.as_view(), name='carrier-calculate'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, View
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+from braces.views import StaffuserRequiredMixin
 
 
 class SearchView(ListView):
@@ -14,3 +18,38 @@ class SearchView(ListView):
         if form.is_valid():
             queryset = form.search()
         return queryset
+
+
+class CarrierView(StaffuserRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        items = request.POST['items']
+        return JsonResponse({
+           "rates": [
+               {
+                   "service_name": "canadapost-overnight",
+                   "service_code": "ON",
+                   "total_price": "1295",
+                   "description": "This is the fastest option by far",
+                   "currency": "CAD",
+                   "min_delivery_date": "2013-04-12 14:48:45 -0400",
+                   "max_delivery_date": "2013-04-12 14:48:45 -0400"
+               },
+               {
+                   "service_name": "fedex-2dayground",
+                   "service_code": "2D",
+                   "total_price": "2934",
+                   "currency": "USD",
+                   "min_delivery_date": "2013-04-12 14:48:45 -0400",
+                   "max_delivery_date": "2013-04-12 14:48:45 -0400"
+               },
+               {
+                   "service_name": "fedex-priorityovernight",
+                   "service_code": "1D",
+                   "total_price": "3587",
+                   "currency": "USD",
+                   "min_delivery_date": "2013-04-12 14:48:45 -0400",
+                   "max_delivery_date": "2013-04-12 14:48:45 -0400"
+               }
+           ]
+        })
